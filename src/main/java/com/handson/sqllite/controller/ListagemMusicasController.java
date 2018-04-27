dbb3052e1d79c7151e68de706f4d5b83b256967f
CASA <CASA@DESKTOP-K7EQLKP>
2018-04-26 21:16:16 -0300

    find by one

--- src/main/java/com/handson/sqllite/controller/ListagemMusicasController.java
+++ src/main/java/com/handson/sqllite/controller/ListagemMusicasController.java
@@ -3,20 +3,22 @@ package com.handson.sqllite.controller;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.handson.sqllite.dao.MusicasDao;
 import com.handson.sqllite.domain.Musicas;
+import com.handson.sqllite.repository.MusicasRepository;
 
 @RestController
 @RequestMapping("/api/musicas")
 public class ListagemMusicasController {
 	
 	@Autowired
-	private MusicasDao musicasDao;
+	private MusicasRepository musicaRepository;
 	
 	@RequestMapping(value="", method = RequestMethod.GET, consumes = {"application/json"})
 	public Collection<Musicas> listagem(@RequestParam(name = "filtro", required = false) String filtro) {
@@ -25,12 +27,9 @@ public class ListagemMusicasController {
 	}
 	
 	@RequestMapping(value="", method = RequestMethod.GET, consumes = {"application/json"})
-	public Musicas findById(@RequestParam(name = "idMusica", required = false) String idMusica) {
-		try {
-			return musicasDao.findOne(Long.valueOf(idMusica));
-		}catch (Exception e) {
-			return null;
-		}
+	public ResponseEntity<Musicas> findById(@RequestParam(name = "idMusica", required = false) String idMusica) {
+		Musicas musicas = musicaRepository.findOne(idMusica);
+		return new ResponseEntity<Musicas>(musicas,HttpStatus.OK);
 		
 	}
 }
