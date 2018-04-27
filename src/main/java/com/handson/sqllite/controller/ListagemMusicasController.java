ef14d97a231db427ee7d7c9de8863d5ea7143a49
Thiago Clemente <thiagobhte2@gmail.com>
2018-04-26 21:11:01 -0300

    Listagem controller

--- src/main/java/com/handson/sqllite/controller/ListagemMusicasController.java
+++ src/main/java/com/handson/sqllite/controller/ListagemMusicasController.java
@@ -1,21 +1,27 @@
 package com.handson.sqllite.controller;
 
 import java.util.Collection;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.handson.sqllite.domain.Musicas;
+import com.handson.sqllite.repository.MusicasRepository;
 
 @RestController
 @RequestMapping("/api/musicas")
 public class ListagemMusicasController {
 	
+	@Autowired
+	private MusicasRepository musicasRepository; 
+	
 	@RequestMapping(value="", method = RequestMethod.GET, consumes = {"application/json"})
 	public Collection<Musicas> listagem(@RequestParam(name = "filtro", required = false) String filtro) {
-		//TODO		
-		return null;
+		List<Musicas> listaMusicas = musicasRepository.findByArtistaNome(filtro);
+		return listaMusicas;
 	}
 }
